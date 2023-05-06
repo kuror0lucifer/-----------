8f8662f31cfc039fae2bf1f3d01f72016113926f
kuror0lucifer <kovalishin.2014@mail.ru>
2023-05-06 18:57:43 +0200

    full redesign, no adaptive

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const special = document.querySelectorAll('.special-number')
 const buttons = document.querySelectorAll('.number');
 const input = document.getElementById('display');
-const sign = document.querySelector('.sign');
-const previous = document.querySelector('.previous')
 const equal = document.querySelector('.equal');
 const percent = document.querySelector('.percent');
 
@@ -20,53 +18,41 @@ for (let i = 0; i < buttons.length; i++) {
 for (let j = 0; j < special.length; j++) {
     special[j].addEventListener('click', function () {
         switch (special[j].textContent) {
-            case 'AC':
+            case 'C':
                 input.textContent = '';
-                sign.textContent = '';
-                previous.textContent = '';
                 break;
-            case '+/-':
-                input.textContent = Number(input.textContent) * -1;
-                break;
+            // case '+/-':
+            //     input.textContent = Number(input.textContent) * -1;
+            //     break;
             case 'x':
-                previous.textContent = `${input.textContent}`;
-                sign.textContent = '*';
-                input.textContent = '';
+                input.textContent += ' * ';
                 break;    
             case '-':
-                previous.textContent = `${input.textContent}`;
-                sign.textContent = '-';
-                input.textContent = '';
+                input.textContent += ' - ';
                 break;
             case '+':
-                previous.textContent = `${input.textContent}`;
-                sign.textContent = '+';
-                input.textContent = '';
+                input.textContent += ' + ';
                 break;
             case '÷':
-                previous.textContent = `${input.textContent}`;
-                sign.textContent = '÷';
-                input.textContent = '';
-
+                input.textContent += ' / ';
+                break;
  
         }
     })
 }
 
 equal.addEventListener('click', function () {
-    let result = eval(`${previous.textContent} ${sign.textContent == '÷' ? '/' : sign.textContent} ${input.textContent}`);
+    let result = eval(`${input.textContent}`);
     if (result.toString().includes('.')) result = result.toFixed(6);
-    sign.textContent = '';
-    previous.textContent = '';
     input.textContent = result;
 
 })
 
-percent.addEventListener('click', function () {
-    let result = previous.textContent * input.textContent / 100;
-    input.textContent = eval(`${previous.textContent} ${sign.textContent} ${result}`);
-    sign.textContent = '' , previous.textContent = '';
-})
+// percent.addEventListener('click', function () {
+//     let result = previous.textContent * input.textContent / 100;
+//     input.textContent = eval(`${previous.textContent} ${sign.textContent} ${result}`);
+//     sign.textContent = '' , previous.textContent = '';
+// })
 
 
 
