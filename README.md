360281895601c4bca0a576bbfd272a55714c768d
kuror0lucifer <132097385+kuror0lucifer@users.noreply.github.com>
2023-05-14 21:11:08 +0200

    full refactoring javascript

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ My first calculator project with adaptive.
 
 I'm going to add some new stuff in the future.
 
-Project link: https://luxury-douhua-dab205.netlify.app
+Project link: https://neon-florentine-355faf.netlify.app
--- index.js
+++ index.js
@@ -1,59 +1,135 @@
 
-const special = document.querySelectorAll('.special-number')
-const buttons = document.querySelectorAll('.number');
-const input = document.getElementById('display');
-const equal = document.querySelector('.equal');
-const percent = document.querySelector('.percent');
-
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
-        if (input.textContent.includes('.') && buttons[i].textContent == '.') {
-            return;
-        }
-        input.textContent += `${this.textContent}`
-    })
-}
-
-for (let j = 0; j < special.length; j++) {
-    special[j].addEventListener('click', function () {
-        switch (special[j].textContent) {
-            case 'C':
-                input.textContent = '';
-                break;
-            // case '+/-':
-            //     input.textContent = Number(input.textContent) * -1;
-            //     break;
-            case 'x':
-                input.textContent += ' * ';
-                break;    
-            case '-':
-                input.textContent += ' - ';
-                break;
+class Calculator {
+    constructor(display) {
+      this.display = display;
+      this.clear();
+    }
+  
+    clear() {
+      this.currentOperand = '';
+      this.previousOperand = '';
+      this.operation = undefined;
+    }
+  
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+  
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return;
+      this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+  
+    chooseOperation(operation) {
+      if (this.currentOperand === '') return;
+      if (this.previousOperand !== '') {
+        this.compute();
+      }
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = '';
+    }
+  
+    compute() {
+      let computation;
+      const prev = parseFloat(this.previousOperand);
+      const current = parseFloat(this.currentOperand);
+      if (isNaN(prev) || isNaN(current)) return;
+      switch (this.operation) {
+        case '+':
+          computation = prev + current;
+          break;
+        case '-':
+          computation = prev - current;
+          break;
+        case 'x':
+          computation = prev * current;
+          break;
+        case '÷':
+          computation = prev / current;
+          break;
+        default:
+          return;
+      }
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = '';
+    }
+  
+    updateDisplay() {
+      this.display.innerText = this.currentOperand;
+    }
+  
+    sum() {
+      this.chooseOperation('+');
+    }
+  
+    subtract() {
+      this.chooseOperation('-');
+    }
+  
+    multiply() {
+      this.chooseOperation('x');
+    }
+  
+    divide() {
+      this.chooseOperation('÷');
+    }
+  
+    computeResult() {
+      this.compute();
+      this.updateDisplay();
+    }
+  
+    addEventListenerToButtons() {
+      const numberButtons = document.querySelectorAll('.number');
+      const operationButtons = document.querySelectorAll('.special-number');
+      const equalsButton = document.querySelector('.equal');
+      const clearButton = document.querySelector('.clear');
+  
+      numberButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          this.appendNumber(button.innerText);
+          this.updateDisplay();
+        });
+      });
+  
+      operationButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          switch (button.innerText) {
             case '+':
-                input.textContent += ' + ';
-                break;
+              this.sum();
+              break;
+            case '-':
+              this.subtract();
+              break;
+            case 'x':
+              this.multiply();
+              break;
             case '÷':
-                input.textContent += ' / ';
-                break;
- 
-        }
-    })
-}
-
-equal.addEventListener('click', function () {
-    let result = eval(`${input.textContent}`);
-    if (result.toString().includes('.')) result = result.toFixed(6);
-    input.textContent = result;
-
-})
-
-// percent.addEventListener('click', function () {
-//     let result = previous.textContent * input.textContent / 100;
-//     input.textContent = eval(`${previous.textContent} ${sign.textContent} ${result}`);
-//     sign.textContent = '' , previous.textContent = '';
-// })
-
+              this.divide();
+              break;
+            default:
+              break;
+          }
+        });
+      });
+  
+      equalsButton.addEventListener('click', () => {
+        this.computeResult();
+      });
+  
+      clearButton.addEventListener('click', () => {
+        this.clear();
+        this.updateDisplay();
+      });
+    }
+  }
+  
+  const display = document.getElementById('display');
+  const calculator = new Calculator(display);
+  calculator.addEventListenerToButtons();
+  
 
 
 
